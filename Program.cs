48aefed0bdc8b493995900b3769da16131735a42
Dave Bohrk <dave@ionicsecurity.com>
2019-11-04 17:36:40 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Eliza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Doctor.Intro());
+
+            var userInput = "";
+            while(!userInput.Equals("bye", StringComparison.OrdinalIgnoreCase))
+            {
+                userInput = Console.ReadLine();
+                string response = Doctor.response(userInput);
+                Console.WriteLine(response);
+            }
+
+        }
+    }
+}
